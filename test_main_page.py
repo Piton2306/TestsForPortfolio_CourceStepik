2300d725ca3e24473330481613d95a03a2343b36
Nastenko Peter <source-306@mail.ru>
2021-12-21 18:31:14 +0300

    Реализация окна регистрации на основе Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,10 @@
+from pages.main_page import MainPage
+import time
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(3)
